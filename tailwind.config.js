17305d489582dafe093a5ee0db4f12af59e73160
navgr2001 <naveenshalom2001@gmail.com>
2025-09-27 18:33:46 +0530

    mobile  app react native

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./App.{js,jsx,ts,tsx}",
+        "./app/**/*.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    presets: [require("nativewind/preset")],
+    theme: { extend: {
+        colors: {primary: '#030014',
+            secondary: '#151312',
+            light:{
+            100: '#d6c6ff',
+                200: '#a8b5db',
+                300: '#9ca4ab',
+            },
+            dark:{
+            100: '#221f3d',
+                200: '#0f0d23',
+            },
+            accent: '#AB8BFF'
+        }
+    }
+    },
+    plugins: []
+};
